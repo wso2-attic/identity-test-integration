37497518d729330f58da4e94fd1c3a45969393bb
nu1silva <nu1silva@gmail.com>
2017-10-09 23:44:30 +0530

    refining tests and common methods

--- commons/src/main/java/org/wso2/qa/commons/operations/Login.java
+++ commons/src/main/java/org/wso2/qa/commons/operations/Login.java
@@ -2,21 +2,20 @@ package org.wso2.qa.commons.operations;
 
 import org.openqa.selenium.WebDriver;
 import org.wso2.qa.commons.pageobjects.LoginPage;
-import org.wso2.qa.commons.testbase.DriverManager;
 import org.wso2.qa.commons.testbase.TestBase;
 
 public class Login {
 
-    TestBase base = new TestBase();
-    DriverManager driverManager = new DriverManager();
+    private TestBase base = new TestBase();
 
-    WebDriver driver;
-
-    public void executeLogin(String username, String password) {
-        driver = driverManager.getWebDriver("chrome");
+    public void executeLogin(String username, String password, WebDriver driver) {
         driver.get(base.getMgtConsoleURL());
         LoginPage.txtBoxUsername(driver).sendKeys(username);
         LoginPage.txtBoxPassword(driver).sendKeys(password);
         LoginPage.btnSignIn(driver).click();
+    }
+
+    public void executeLogout(WebDriver driver) {
+        driver.get(base.getMgtConsoleLogout());
     }
 }
--- solution18/src/test/java/org/wso2/qa/solution18/LoginTest.java
+++ solution18/src/test/java/org/wso2/qa/solution18/LoginTest.java
@@ -2,24 +2,40 @@ package org.wso2.qa.solution18;
 
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
-import org.wso2.qa.commons.pageobjects.LoginPage;
+import org.wso2.qa.commons.operations.Login;
+import org.wso2.qa.commons.pageobjects.MenuItems;
 import org.wso2.qa.commons.testbase.TestBase;
 
 public class LoginTest {
 
-    private TestBase base = new TestBase();
-    org.wso2.qa.commons.testbase.DriverManager driverManager = new org.wso2.qa.commons.testbase.DriverManager();
+    private org.wso2.qa.commons.testbase.DriverManager driverManager = new org.wso2.qa.commons.testbase.DriverManager();
+    private Login login = new Login();
 
-    WebDriver driver;
+    private WebDriver driver;
 
     @Test
-    public void loginTest() {
+    public void loginTest() throws InterruptedException {
         String username = "admin";
         String password = "admin";
         driver = driverManager.getWebDriver("chrome");
-        driver.get(base.getMgtConsoleURL());
-        LoginPage.txtBoxUsername(driver).sendKeys(username);
-        LoginPage.txtBoxPassword(driver).sendKeys(password);
-        LoginPage.btnSignIn(driver).click();
+        login.executeLogin(username, password, driver);
+
+        // check menu items
+        MenuItems.tabMain(driver).click();
+        MenuItems.tabMonitor(driver).click();
+        MenuItems.tabConfigure(driver).click();
+        MenuItems.tabTools(driver).click();
+
+        // check main menu items
+        MenuItems.tabMain(driver).click();
+        MenuItems.userNrolesAdd(driver).click();
+        MenuItems.userNrolesList(driver).click();
+        MenuItems.userStoresAdd(driver).click();
+        MenuItems.userStoresList(driver).click();
+        MenuItems.claimsAdd(driver).click();
+        MenuItems.claimsList(driver).click();
+
+        login.executeLogout(driver);
+        driver.close();
     }
 }
