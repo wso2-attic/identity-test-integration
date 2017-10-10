6ea6d1525ac23ec2299e00fb7ba32ec07d74f072
nu1silva <nu1silva@gmail.com>
2017-10-10 15:30:33 +0530

    copyrights updated and introduced property file to get the locators

--- commons/src/main/java/org/wso2/qa/commons/pageobjects/ServiceProviderConfigurePage.java
+++ commons/src/main/java/org/wso2/qa/commons/pageobjects/ServiceProviderConfigurePage.java
@@ -1,9 +1,32 @@
+/*
+ *  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
 package org.wso2.qa.commons.pageobjects;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+/**
+ * Service Provider Configuration Page locator class
+ * <p>
+ * TODO - move element values into the property file
+ */
 public class ServiceProviderConfigurePage {
 
     private static WebElement element = null;
@@ -22,6 +45,10 @@ public class ServiceProviderConfigurePage {
         element = driver.findElement(By.xpath("//*[@id=\"isSaasApp\"]"));
         return element;
     }
+
+    /*
+     * Configuration tabs
+     */
 
     public static WebElement tabClaimConfiguration(WebDriver driver) {
         element = driver.findElement(By.xpath
@@ -59,3 +86,4 @@ public class ServiceProviderConfigurePage {
         return element;
     }
 }
+
--- commons/src/main/java/org/wso2/qa/commons/pageobjects/ServiceProviderCreatePage.java
+++ commons/src/main/java/org/wso2/qa/commons/pageobjects/ServiceProviderCreatePage.java
@@ -1,32 +1,57 @@
+/*
+ *  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
 package org.wso2.qa.commons.pageobjects;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.wso2.qa.commons.util.LocatorProperties;
 
+/**
+ * Service Provider creator page locator class
+ * <p>
+ */
 public class ServiceProviderCreatePage {
 
     private static WebElement element = null;
 
     public static WebElement txtBoxServiceProviderName(WebDriver webDriver) {
-        element = webDriver.findElement(By.xpath("//*[@id=\"spName\"]"));
+        element = webDriver.findElement(By.xpath
+                (LocatorProperties.getInstance().getElement("page.spcreate.txtbox.spname")));
         return element;
     }
 
     public static WebElement txtBoxServiceProvierDescription(WebDriver webDriver) {
-        element = webDriver.findElement(By.xpath("//*[@id=\"sp-description\"]"));
+        element = webDriver.findElement(By.xpath
+                (LocatorProperties.getInstance().getElement("page.spcreate.txtbox.spdescription")));
         return element;
     }
 
     public static WebElement btnRegister(WebDriver webDriver) {
         element = webDriver.findElement(By.xpath
-                ("/html/body/table/tbody/tr[2]/td[3]/table/tbody/tr[2]/td/div/div/form/div[3]/input[1]"));
+                (LocatorProperties.getInstance().getElement("page.spcreate.btn.register")));
         return element;
     }
 
     public static WebElement btnCancel(WebDriver webDriver) {
-        element = webDriver.findElement
-                (By.xpath("/html/body/table/tbody/tr[2]/td[3]/table/tbody/tr[2]/td/div/div/form/div[3]/input[2]"));
+        element = webDriver.findElement(By.xpath
+                (LocatorProperties.getInstance().getElement("page.spcreate.btn.cancel")));
         return element;
     }
 }
