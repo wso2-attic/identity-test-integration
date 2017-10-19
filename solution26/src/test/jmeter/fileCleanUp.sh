25a89f4e9cc71d4c0f35916aaa51cf46b5ae9e83
aparnawso2 <aparna@wso2.com>
2017-10-19 11:56:13 +0530

    adding user management script

---
+++ solution26/src/test/jmeter/fileCleanUp.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "Clear Role list"
+cp /dev/null /home/aparna/QAHackathon/IAM/JmeterScripts/roleList.csv
+
+echo "Clear User List"
+cp /dev/null /home/aparna/QAHackathon/IAM/JmeterScripts/usersList.csv
