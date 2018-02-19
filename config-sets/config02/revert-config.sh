b3386961fb4b0c17c57d78cd4e54294c3ca0aa17
shanika <shanikaw@wso2.com>
2018-02-19 17:29:03 +0530

    Add config revert shell script and rename config.sh to apply-config.sh in sol 5

---
+++ config-sets/config02/revert-config.sh
@@ -0,0 +1,78 @@
+#include the path of the IS Pack for the productHome variable when trying locally
+productHome=$productHome
+
+serverHost=$serverHost
+serverPort=$serverPort
+
+file=$productHome/repository/conf/security/authenticators.xml
+
+xmlstarlet edit -L -N w=http://wso2.org/projects/carbon/authenticators.xml \
+-u "/w:Authenticators/w:Authenticator[@name='SAML2SSOAuthenticator'] \
+/w:Priority" -v "10" $file
+
+xmlstarlet edit -L -N w=http://wso2.org/projects/carbon/authenticators.xml \
+-u "/w:Authenticators/w:Authenticator[@name='SAML2SSOAuthenticator'] \
+/@disabled" -v "true" $file
+
+echo "Values added to the file: $file"
+
+cd $productHome/bin
+
+#verify file copy status and exit on failure
+statusval=$?
+if [ $statusval -eq 0 ]; then
+ echo "file copying success!"
+ #stop the server
+ echo "shutting down the server now..."
+ sh wso2server.sh stop
+else
+    echo "file copy command failed..."
+    exit 1
+fi
+#wait till server stops
+y=0;
+retry_count1=20;
+while true
+do
+echo $(date)" Waiting until server stops"
+if curl -s -w '%{http_code}' https://$serverHost:$serverPort/carbon -k | grep "000"
+then
+ echo "Carbon server stopped...!"
+ break
+else
+ echo "Carbon server is still running..."
+    if [ $y = $retry_count1 ]; then
+    echo "Couldn't stop the server"
+        exit 1
+    fi
+fi
+y=$((y+1))
+sleep 1
+done
+#wait few seconds to finish with server-stop
+sleep 3
+
+#start back the server
+echo "server starting..."
+sh wso2server.sh start
+#wait till server starts
+x=0;
+retry_count=20;
+while true
+do
+echo $(date)" waiting until server starts..."
+if curl -s -w '%{http_code}' https://$serverHost:$serverPort/carbon -k | grep "302"
+then
+ echo "found server running..."
+ echo "configurations Done...!"
+ exit 0
+else
+ echo "carbon server not running..."
+    if [ $x = $retry_count ]; then
+    echo "Couldn't start the server"
+        exit 1
+    fi
+fi
+x=$((x+1))
+sleep 3
+done
