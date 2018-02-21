455eb0a704c963000c6fc00f9447daccad86fc01
pubudup <pubudup@wso2.com>
2018-02-21 11:24:04 +0530

    renamed pre and post config files as described in the git doc. Added post config file to revert applied product configs

---
+++ config-sets/config01/revert-config.sh
@@ -0,0 +1,88 @@
+$productHome
+filePath=/repository/deployment/server/webapps/authenticationendpoint
+
+prgdir=$(dirname "$0")
+configDir=$(cd "$prgdir"; pwd)
+
+
+#remove config files
+#cd $productHome/repository/deployment/server/webapps/
+#mkdir authenticationendpoint
+cd $productHome
+rm $productHome/$filePath/pwd-reset.jsp
+echo "pwd-reset.jsp file removed from path: $productHome/$filePath"
+
+
+#build the product
+
+rm $productHome/repository/components/dropins/org.wso2.carbon.extension.identity.authenticator.passwordpolicy.connector-*.jar
+echo "Authenticator jar is removed from dropins folder"
+
+
+#update identity-mgt.properties
+value="Authentication.Policy.Password.Reset.Time.In.Days=20"
+file=identity-mgt.properties
+sed -ie '$d' $file
+
+
+echo "Value; $value removed from the file: $file"
+
+cd $productHome/bin
+
+#verify file copy status and exit on failure
+statusval=$?
+if [ $statusval -eq 0 ]; then
+ echo "file copying success!"
+ #stop the server
+ echo "shutting down the server now..."
+ sh wso2server.sh stop
+else
+    echo "file copy command failed..."
+    exit 1
+fi
+#wait till server stops
+y=0;
+retry_count1=20;
+while true
+do
+echo $(date)" Waiting until server stops"
+if curl -s -w '%{http_code}' https://$serverHost:$serverPort/carbon -k | grep "000"
+then
+ echo "Carbon server stopped...!"
+ break
+else
+ echo "Carbon server is still running..."
+    if [ $y = $retry_count1 ]; then
+    echo "Couldn't stop the server"
+        exit 1
+    fi
+fi
+x=$((x+1))
+sleep 1
+done
+#wait few seconds to finish with server-stop
+sleep 3
+#start back the server
+echo "server starting..."
+sh wso2server.sh start
+#wait till server starts
+x=0;
+retry_count=20;
+while true
+do
+echo $(date)" waiting until server starts..."
+if curl -s -w '%{http_code}' https://$serverHost:$serverPort/carbon -k | grep "302"
+then
+ echo "found server running..."
+ echo "configurations Done...!"
+ exit 0
+else
+ echo "carbon server not running..."
+    if [ $x = $retry_count ]; then
+    echo "Couldn't start the server"
+        exit 1
+    fi
+fi
+x=$((x+1))
+sleep 3
+done
