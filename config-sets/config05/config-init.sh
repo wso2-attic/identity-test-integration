2bee62d431aed38812606b3c44b41272ae62b98d
shavantha weerasinghe <shavantha@gmail.com>
2018-06-15 18:08:48 +0530

    config set for scenario27

---
+++ config-sets/config05/config-init.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# Copyright (c) 2018, WSO2 Inc. (http://wso2.com) All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#use sudo su command and hit enter to give root permissions
+#run the file with the command sh config-init.sh
+
+
+#xml starlet will be installed with the Y option selected through the shell script for the question Do you want to continue?
+
+if command -v xmlstarlet; then
+	echo "xmlstarlet exists!"
+  	exit 0
+else
+	echo "Module is Installing"
+	sudo apt-get -y install xmlstarlet
+fi
+if command -v xmlstarlet; then
+	echo "Successfully installed"
+  	exit 0
+else
+	echo "installation failed"
+	exit 1
+fi
