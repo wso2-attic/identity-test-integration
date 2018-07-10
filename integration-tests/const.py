bab18dbe54497e0f9caf7e6599ae7e9f845bad3e
chaminda@wso2.com <chaminda@wso2.com>
2018-07-10 16:00:23 +0530

    add updated python scripts for integration tests

---
+++ integration-tests/const.py
@@ -0,0 +1,58 @@
+# Copyright (c) 2018, WSO2 Inc. (http://wso2.com) All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+NS = {'d': 'http://maven.apache.org/POM/4.0.0'}
+ZIP_FILE_EXTENSION = ".zip"
+CARBON_NAME = "carbon.zip"
+VALUE_TAG = "{http://maven.apache.org/POM/4.0.0}value"
+SURFACE_PLUGIN_ARTIFACT_ID = "maven-surefire-plugin"
+DATASOURCE_PATHS = ["repository/conf/datasources/master-datasources.xml",
+                    "repository/conf/datasources/metrics-datasources.xml", "repository/conf/datasources/bps-datasources.xml"]
+POM_FILE_PATHS = ["modules/integration/tests-integration/tests-backend/pom.xml",
+                  "modules/integration/tests-ui-integration/pom.xml",
+                  "modules/integration/tests-common/pom.xml"]
+DIST_POM_PATH = {"product-is": "modules/distribution/pom.xml", "product-apim": "modules/distribution/product/pom.xml",
+                 "product-ei": "distribution/pom.xml"}
+LIB_PATH = "repository/components/lib"
+DISTRIBUTION_PATH = "modules/distribution/product/target"
+PRODUCT_STORAGE_DIR_NAME = "storage"
+TEST_PLAN_PROPERTY_FILE_NAME = "testplan-props.properties"
+INFRA_PROPERTY_FILE_NAME = "infrastructure.properties"
+LOG_FILE_NAME = "integration.log"
+ORACLE_DB_ENGINE = "ORACLE-SE2"
+MYSQL_DB_ENGINE = "MYSQL"
+DEFAULT_ORACLE_SID = "orcl"
+DB_CARBON_DB = 'WSO2_CARBON_DB'
+DB_AM_DB = 'WSO2AM_DB'
+DB_STAT_DB = 'WSO2AM_STATS_DB'
+DB_BPS_DB = 'BPS_DS'
+DB_METRICS_DB = 'WSO2_METRICS_DB'
+DEFAULT_DB_USERNAME = "wso2carbon"
+LOG_STORAGE = "logs"
+LOG_FILE_PATHS = ["modules/integration/tests-integration/tests-backend/target/surefire-reports/emailable-report.html",
+                  "modules/integration/tests-integration/tests-backend/target/surefire-reports/index.html",
+                  "modules/integration/tests-integration/tests-backend/target/surefire-reports/TEST-TestSuite.xml",
+                  "modules/integration/tests-integration/tests-backend/target/surefire-reports/testng.css",
+                  "modules/integration/tests-integration/tests-backend/target/surefire-reports/testng-results.xml",
+                  "modules/integration/tests-integration/tests-backend/target/surefire-reports/TestSuite.txt",
+                  "modules/integration/tests-integration/tests-backend/target/logs/automation.log"]
+DB_META_DATA = {
+    "MYSQL": {"prefix": "jdbc:mysql://", "driverClassName": "com.mysql.jdbc.Driver", "jarName": "mysql.jar"},
+    "SQLSERVER-SE": {"prefix": "jdbc:sqlserver://",
+                     "driverClassName": "com.microsoft.sqlserver.jdbc.SQLServerDriver", "jarName": "sqlserver-ex.jar"},
+    "ORACLE-SE2": {"prefix": "jdbc:oracle:thin:@", "driverClassName": "oracle.jdbc.OracleDriver",
+                   "jarName": "oracle-se.jar"},
+    "POSTGRESQL": {"prefix": "jdbc:postgresql://", "driverClassName": "org.postgresql.Driver",
+                   "jarName": "postgres.jar"}
+}
